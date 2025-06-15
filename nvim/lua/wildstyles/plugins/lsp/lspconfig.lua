d0287c472c9321f3af63398919169d79aa03882f
Ruslan Vanzhula <r.vanzhula@gmail.com>
2025-06-15 17:04:04 +0300

    adjusted diagnostics

--- nvim/lua/wildstyles/plugins/lsp/lspconfig.lua
+++ nvim/lua/wildstyles/plugins/lsp/lspconfig.lua
@@ -44,18 +44,6 @@ return {
         opts.desc = "Smart rename"
         keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-        opts.desc = "Show line diagnostics"
-        keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-        opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-        opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
         -- opts.desc = "Show documentation for what is under cursor"
         -- keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 

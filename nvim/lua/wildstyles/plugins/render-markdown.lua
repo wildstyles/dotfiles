955f7610115137aec995216e8724426f77366c69
Ruslan Vanzhula <r.vanzhula@gmail.com>
2025-08-14 19:33:24 +0300

    render markdown adjustments

--- nvim/lua/wildstyles/plugins/render-markdown.lua
+++ nvim/lua/wildstyles/plugins/render-markdown.lua
@@ -1,11 +1,79 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-	config = function()
-		require("render-markdown").setup({
-			completions = { blink = { enabled = true } },
-		})
-	end,
+	enabled = true,
+	-- Moved highlight creation out of opts as suggested by plugin maintainer
+	-- There was no issue, but it was creating unnecessary noise when ran
+	-- :checkhealth render-markdown
+	-- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/138#issuecomment-2295422741
+	opts = {
+		bullet = {
+			-- Turn on / off list bullet rendering
+			enabled = true,
+		},
+		checkbox = {
+			-- Turn on / off checkbox state rendering
+			enabled = true,
+			-- Determines how icons fill the available space:
+			--  inline:  underlying text is concealed resulting in a left aligned icon
+			--  overlay: result is left padded with spaces to hide any additional text
+			position = "inline",
+			unchecked = {
+				-- Replaces '[ ]' of 'task_list_marker_unchecked'
+				icon = "   󰄱 ",
+				-- Highlight for the unchecked icon
+				highlight = "RenderMarkdownUnchecked",
+				-- Highlight for item associated with unchecked checkbox
+				scope_highlight = nil,
+			},
+			checked = {
+				-- Replaces '[x]' of 'task_list_marker_checked'
+				icon = "   󰱒 ",
+				-- Highlight for the checked icon
+				highlight = "RenderMarkdownChecked",
+				-- Highlight for item associated with checked checkbox
+				scope_highlight = nil,
+			},
+		},
+		html = {
+			-- Turn on / off all HTML rendering
+			enabled = true,
+			comment = {
+				-- Turn on / off HTML comment concealing
+				conceal = false,
+			},
+		},
+		-- Add custom icons lamw26wmal
+		link = {
+			image = vim.g.neovim_mode == "skitty" and "" or "󰥶 ",
+			custom = {
+				youtu = { pattern = "youtu%.be", icon = "󰗃 " },
+			},
+		},
+		heading = {
+			sign = false,
+			icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+			backgrounds = {
+				"Headline1Bg",
+				"Headline2Bg",
+				"Headline3Bg",
+				"Headline4Bg",
+				"Headline5Bg",
+				"Headline6Bg",
+			},
+			foregrounds = {
+				"Headline1Fg",
+				"Headline2Fg",
+				"Headline3Fg",
+				"Headline4Fg",
+				"Headline5Fg",
+				"Headline6Fg",
+			},
+		},
+		code = {
+			-- if I'm not using yabai, I cannot make the color of the codeblocks
+			-- transparent, so just disabling all rendering 😢
+			width = "block",
+			highlight = "RenderMarkdownCodeInline",
+		},
+	},
 }

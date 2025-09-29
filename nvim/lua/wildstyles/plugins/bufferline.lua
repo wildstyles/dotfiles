84813aca97e9757555e5fc65adb87487feadcca4
Ruslan Vanzhula <r.vanzhula@gmail.com>
2025-09-29 09:47:27 +0300

    adjusted kulala

--- nvim/lua/wildstyles/plugins/bufferline.lua
+++ nvim/lua/wildstyles/plugins/bufferline.lua
@@ -1,12 +1,12 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      always_show_bufferline = false,
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			always_show_bufferline = false,
+			mode = "tabs",
+			separator_style = "slant",
+		},
+	},
 }
--- nvim/lua/wildstyles/plugins/kulala.lua
+++ nvim/lua/wildstyles/plugins/kulala.lua
@@ -7,7 +7,27 @@ return {
 	},
 	ft = { "http", "rest" },
 	opts = {
-		global_keymaps = true,
+		request_timout = 10000,
+		-- global_keymaps = true,
+		global_keymaps = {
+			["Send all requests"] = false,
+			-- ["Select environment"] = false,
+			["Open kulala"] = false,
+			["Download GraphQL schema"] = false,
+			["Toggle headers/body"] = false,
+			["Open scratchpad"] = false,
+			["Close window"] = false,
+			["Jump to next request"] = false,
+			["Jump to previous request"] = false,
+			["Send request"] = { -- sets global mapping
+				"<CR>",
+				function()
+					require("kulala").run()
+				end,
+				mode = { "n", "v" }, -- optional mode, default is n
+				desc = "Send request changed description", -- optional description, otherwise inferred from the key
+			},
+		},
 		global_keymaps_prefix = "<leader>R",
 		kulala_keymaps_prefix = "",
 	},

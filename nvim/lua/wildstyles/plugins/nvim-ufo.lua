4f461e04a4f1f107c7161cfea24048b9f611faab
Ruslan Vanzhula <r.vanzhula@gmail.com>
2025-10-19 19:45:11 +0300

    .md files folding fix

--- nvim/lua/wildstyles/plugins/nvim-ufo.lua
+++ nvim/lua/wildstyles/plugins/nvim-ufo.lua
@@ -2,26 +2,42 @@ return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = "kevinhwang91/promise-async",
 	config = function()
-		vim.o.foldcolumn = "0" -- '0' is not bad
+		vim.o.foldcolumn = "1" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
 
 		-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-		vim.keymap.set("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
-		vim.keymap.set("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
-		vim.keymap.set("n", "zK", function()
-			local winid = require("ufo").peekFoldedLinesUnderCursor()
-			if not winid then
-				vim.lsp.buf.hover()
-			end
-		end, { desc = "Peek Fold" })
+		vim.keymap.set(
+			"n",
+			"zR",
+			require("ufo").openAllFolds,
+			{ desc = "Open all folds" }
+		)
+		vim.keymap.set(
+			"n",
+			"zM",
+			require("ufo").closeAllFolds,
+			{ desc = "Close all folds" }
+		)
+		-- vim.keymap.set("n", "zK", function()
+		-- 	local winid = require("ufo").peekFoldedLinesUnderCursor()
+		-- 	if not winid then
+		-- 		vim.lsp.buf.hover()
+		-- 	end
+		-- end, { desc = "Peek Fold" })
+		--
+		local ftMap = {
+			lua = { "lsp", "indent" },
+			javascript = { "lsp", "indent" },
+			markdown = "", -- folding works correnctly only in this way for .md
+		}
 
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
-				return { "lsp", "indent" }
+				print(filetype)
+				return ftMap[filetype]
 			end,
 		})
 	end,
 }
---
--- nvim/lua/wildstyles/plugins/statuscol.lua
+++ nvim/lua/wildstyles/plugins/statuscol.lua
@@ -1,18 +1,52 @@
 return {
-  "luukvbaal/statuscol.nvim",
-  config = function()
-    local builtin = require("statuscol.builtin")
-    require("statuscol").setup({
-      relculright = true,
-      segments = {
-        -- { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-        { sign = { namespace = { "diagnostic.signs" }, maxwidth = 1, colwidth = 2, auto = false } },
-        -- { text = { " " } },
-        { text = { builtin.lnumfunc } },
-        {
-          sign = { auto = false, colwidth = 2, maxwidth = 1, namespace = { "gitsigns" }, name = { ".*" } },
-        },
-      },
-    })
-  end,
+	"luukvbaal/statuscol.nvim",
+	config = function()
+		local builtin = require("statuscol.builtin")
+
+		require("statuscol").setup({
+			relculright = true,
+			segments = {
+
+				{
+					sign = {
+						namespace = { "diagnostic.signs" },
+						foldclosed = true,
+						maxwidth = 1,
+						colwidth = 2,
+						auto = false,
+					},
+				},
+
+				{ text = { " " } },
+
+				{ text = { builtin.lnumfunc } },
+
+				{
+					sign = {
+						auto = false,
+						colwidth = 2,
+						maxwidth = 1,
+						namespace = { "gitsigns" },
+						name = { ".*" },
+					},
+				},
+
+				-- {
+				-- 	text = {
+				-- 		function(args)
+				-- 			-- args.fold.close = ""
+				-- 			-- args.fold.open = ""
+				-- 			-- args.fold.open = " "
+				-- 			args.fold.sep = " "
+				-- 			return builtin.foldfunc(args)
+				-- 		end,
+				-- 	},
+				-- 	colwidth = 1,
+				-- 	click = "v:lua.ScFa",
+				-- },
+				--
+				-- { text = { " " } },
+			},
+		})
+	end,
 }

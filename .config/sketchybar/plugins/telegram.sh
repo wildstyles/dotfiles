464c3ddda15670b110cf1f7d681aa67b9a706d28
Ruslan Vanzhula <r.vanzhula@gmail.com>
2025-12-07 16:29:56 +0200

    telegram sketchybar icon after system reboot fix

--- .config/sketchybar/plugins/telegram.sh
+++ .config/sketchybar/plugins/telegram.sh
@@ -3,22 +3,31 @@
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Telegram")
-if [[ $STATUS_LABEL =~ \"label\"=([^[:space:]{}]+) ]]; then
+STATUS_LABEL=$(echo "$STATUS_LABEL" | tr -d '\r' | sed 's/^[[:space:]]*//;s/[[:space:]]*$//' | tr -d '\n')
+# that's the value after system reboot
+EXPECTED_LABEL='"StatusLabel"=[ NULL ]'
+
+if [[ "$STATUS_LABEL" == "$EXPECTED_LABEL" ]]; then
+  ICON_COLOR=$WHITE
+  LABEL=""
+else 
+  if [[ $STATUS_LABEL =~ \"label\"=([^[:space:]{}]+) ]]; then
     LABEL="${BASH_REMATCH[1]}"
     LABEL=${LABEL//\"/}
 
     if [[ $LABEL == kCFNULL ]]; then
-        ICON_COLOR=$WHITE
-        LABEL=""
+      ICON_COLOR=$WHITE
+      LABEL=""
     elif [[ $LABEL == "•" ]]; then
-        ICON_COLOR=$YELLOW
+      ICON_COLOR=$YELLOW
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
-        ICON_COLOR=$GREEN
+      ICON_COLOR=$GREEN
     else
-        exit 0
+      exit 0
     fi
-else
+  else
     exit 0
+  fi
 fi
 
 sketchybar --set "$NAME" icon=":telegram:" label="$LABEL" icon.color="$ICON_COLOR"
